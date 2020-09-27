926c6185ebbe88993c01570029d6b7858fd490cb
abhisheks <abhisheks@netlink.com>
2020-09-27 13:56:54 +0530

    added notes column

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ app.get('/get', (req, resp) => {
     resp.send({ data: "Hello World" })
 })
 
-app.get('/', (req, resp) => {
-    resp.send({ data: "Welcome Home" })
+app.get('/get-notes', (req, resp) => {
+    var notes = [
+        {
+            title: "new one",
+            desc: "added dummy text"
+        }
+    ]
+    resp.send({ notes: notes })
 })
 
 app.listen(process.env.PORT || 3001)
