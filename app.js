cb42ae8c630dcf83c0210a58f5986e506b77164b
abhisheks <abhisheks@netlink.com>
2020-10-04 21:34:00 +0530

    changes

---
+++ app.js
@@ -0,0 +1,63 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from "body-parser"
+import mongoose from 'mongoose'
+import morgan from 'morgan'
+
+let app = express()
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors())
+app.use(morgan('tiny'))
+
+const uri = "mongodb+srv://abhisheks:admin@cluster0.gr3w7.mongodb.net/personal?retryWrites=true&w=majority";
+
+// schema defining
+const Schema = mongoose.Schema
+const notesSchema = new Schema({
+  title: String,
+  desc: String,
+  lastModified: Number
+})
+
+// Model and will be used for CRUD operations
+const notesModel = mongoose.model("notesModel", notesSchema)
+
+// DB connection
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+// checking connection
+mongoose.connection.on('connected', () => {
+  console.log("Connected to database")
+})
+
+// routes
+app.get('/get-notes', async (req, resp) => {
+  let response;
+  await notesModel.find({}).then(data => {
+    response = data
+  })
+  .catch(err => {
+    response = []
+  })
+  resp.send({ notes: response })
+})
+
+app.post('/save-note', (req, resp) => {
+  let {title, desc} = req.body
+  const notes = new notesModel({
+    title,
+    desc,
+    // lastModified: new Date().toTimeString
+  })
+  notes.save(error => {
+    if (error) console.log("error occured")
+    else console.log("added successfully", error)
+    resp.send({error: false})
+  })
+})
+
+app.listen(process.env.PORT || 3005)
